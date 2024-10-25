cfbec83a2fe80880e813ebf09669b9888aa40bbc
rms2003 <gamermls630@gmail.com>
2024-10-25 00:40:24 -0500

    Update db.php

--- try/db.php
+++ try/db.php
@@ -1,5 +1,5 @@
 <?php
-$connection = new mysqli('localhost', 'root', '', 'task_manager'); // Adjust credentials if needed
+$connection = new mysqli('localhost', 'root', '', 'task_manager');
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
   }
